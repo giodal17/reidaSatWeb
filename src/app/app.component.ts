983c42213005a41a2273d1a9853e913978291bad
Giovanni <g.dalessandro@almaviva.it>
2024-01-30 16:34:23 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reidaSatWeb';
+  form: FormGroup;
+  numTentativi:number = 0;
+  varStorageNumTentativi: string | null= "";
+  success = false;
+  constructor(public fb: FormBuilder) {
+    this.form = this.fb.group({
+      risposta: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    if(isNaN(this.getNumTentativifromStorage())){  
+      localStorage.setItem("numTentativi", "3")
+    }
+    this.success = (localStorage.getItem("success") === 'true')
+    this.numTentativi = this.getNumTentativifromStorage();
+  }
+
+  onSubmit(form: FormGroup) {
+    if(form.value.risposta){
+      let risposta: string = form.value.risposta
+      if(risposta.toLowerCase() == "reida"){
+        localStorage.setItem("numTentativi", "0");
+        this.numTentativi = 0;
+        localStorage.setItem("success", "true");
+        this.success = true;
+        return;
+      }
+      
+    }
+    this.numTentativi--;
+    this.setNumTentativiInStorage();
+    this.numTentativi = this.getNumTentativifromStorage();
+    form.reset();
+    }
+
+    getNumTentativifromStorage(){
+      this.varStorageNumTentativi = localStorage.getItem("numTentativi");
+      return parseInt(this.varStorageNumTentativi!);
+    }
+    setNumTentativiInStorage(){
+      this.varStorageNumTentativi = this.numTentativi.toString();
+      localStorage.setItem("numTentativi", this.varStorageNumTentativi);
+    }
 }
